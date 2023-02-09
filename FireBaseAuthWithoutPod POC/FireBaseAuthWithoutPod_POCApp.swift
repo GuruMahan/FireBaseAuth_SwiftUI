d9942388eb154101288f0e2433d7f55d0226e42b
GuruMahan <iosdeve2022@gmail.com>
2023-02-09 15:18:16 +0530

    Initial Commit

---
+++ FireBaseAuthWithoutPod POC/FireBaseAuthWithoutPod_POCApp.swift
@@ -0,0 +1,17 @@
+//
+//  FireBaseAuthWithoutPod_POCApp.swift
+//  FireBaseAuthWithoutPod POC
+//
+//  Created by Guru Mahan on 09/02/23.
+//
+
+import SwiftUI
+
+@main
+struct FireBaseAuthWithoutPod_POCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
